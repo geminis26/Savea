895ba607ea68d42170dcb5a69c048a766524d511
g3minis26 <g3minis26@gmail.com>
2018-06-26 17:30:02 -0500

    Agregar archivos de proyecto.

---
+++ Savea/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Savea.Startup))]
+namespace Savea
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
